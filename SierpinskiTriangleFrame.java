e39a0ac56eee0210b5614834183f67d05f24c907
simay2 <72138608+simay2@users.noreply.github.com>
2021-03-09 22:08:31 +0200

    Add files via upload

--- SierpinskiTriangleFrame.java
+++ SierpinskiTriangleFrame.java
@@ -7,40 +7,55 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+
 public class SierpinskiTriangleFrame extends JFrame {
-	static int r = 0, gr = 0, b = 255,orders=0;
-
+	//static int r = 0, gr = 0, b = 255;
+	//private JTextField txtOrder = new JTextField("0", 5); // Order
 	private SierpinskiTrianglePanel trianglePanel = new SierpinskiTrianglePanel(); // To display the pattern
-
+	
+	JButton plusButton = new JButton("+");
+	JButton minusButton = new JButton("-");
+	
 	public SierpinskiTriangleFrame() {
-// Panel to hold label, text field, and a button
 		JPanel panel = new JPanel();
-		
-		JButton plusButton = new JButton("+");
 		panel.add(plusButton);
+		panel.add(minusButton);
+		
+		//panel.add(new JLabel("Enter an order: "));
+		//panel.add(txtOrder);
+		//txtOrder.setHorizontalAlignment(SwingConstants.RIGHT);
+
+		add(trianglePanel);
+		add(panel, BorderLayout.SOUTH);
+
+		/*txtOrder.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				trianglePanel.setOrder(Integer.parseInt(txtOrder.getText()));
+			}
+		});*/
 		
 		plusButton.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				orders++;
-				trianglePanel.setOrder(orders);
+			public void actionPerformed(ActionEvent arg0) {
+				trianglePanel.increaseOrder();
 			}
 		});
-		JButton minusButton = new JButton("-");
-		panel.add(minusButton);
 		
 		minusButton.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				orders--;
-				if(orders<0) {
-					orders=0;
-				}
-				trianglePanel.setOrder(orders);
+			public void actionPerformed(ActionEvent arg0) {
+				trianglePanel.decreaseOrder();
 			}
 		});
-		add(trianglePanel);
-		add(panel, BorderLayout.SOUTH);
 
 	}
 
